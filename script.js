4bb00514aa9295d4e27d189dd1ff6aa38beb333d
nicola155 <edrisbadronjr1@gmail.com>
2023-12-04 14:45:17 +0800

    15th commit

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ function closeModal() {
     var modal = document.getElementById('myModal');
     modal.style.display = 'none';
 }
+
+
+// Open the gmail for contact me
+document.getElementById("openGmailPopupBtn").addEventListener("click", function() {
+   
+    var emailAddress = "kalbojakerss123@gmail.com";
+    var subject = "Your Topic";
+    var body = "Your email body content";
+
+   
+    var gmailComposeUrl = "https://mail.google.com/mail/?view=cm&to=" + encodeURIComponent(emailAddress) +
+                          "&su=" + encodeURIComponent(subject) +
+                          "&body=" + encodeURIComponent(body);
+
+    
+    window.open(gmailComposeUrl, "_blank");
+  });
